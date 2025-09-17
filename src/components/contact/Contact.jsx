3778411101aeeb4a3d23fb80072169c63ea71ead
anurag2787 <anuragyadav2787@gmail.com>
2025-09-17 23:34:56 +0530

    Revert "FIxed to center"
    This reverts commit 7657d900311bd6749b10e4063a12a21cf169e3f1.

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -67,9 +67,12 @@ export default function Contact() {
  
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-[#F8A199] p-4">
+    <div className="flex justify-center bg-[#F8A199] sm:items-center sm:pt-0 md:mx-0 mx-5">
+      {/* <div className=" mx-auto sm:px-6 lg:px-8"> */}
+        {/* <div className="mt-8 overflow-hidden"> */}
+          {/* <div className=" grid grid-cols-1 md:grid-cols-2 bg-[#ffdad7] p-8 rounded-lg"> */}
           {!check ? (
-            <div className="grid grid-cols-1 md:grid-cols-2 bg-[#ffdad7] p-8 rounded-lg w-full max-w-7xl">
+            <div className="grid grid-cols-1 md:grid-cols-2 bg-[#ffdad7] p-8 rounded-lg md:my-32 my-10">
               <div className="left p-0 md:p-6 mr-2  sm:rounded-lg md:shadow-xl">
                 <h1 className="text-3xl sm:text-4xl text-black font-extrabold tracking-tight">
                   Get in touch:
@@ -205,7 +208,7 @@ export default function Contact() {
               </form>
             </div>
           ) : (
-            <div className="bg-[#ffdad7] p-8 rounded-lg w-full max-w-2xl">
+            <div className="bg-[#ffdad7] p-8 rounded-lg ">
               <div className="flex flex-col items-center">
                 <img
                   src={GIF}
