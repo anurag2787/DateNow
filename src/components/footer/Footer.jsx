224499fbed8299f00497407a1b6d892d3a08e919
Anupriya Soni <anupriyasoni204@gmail.com>
2025-09-19 16:45:59 +0530

    Updated again

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
     <footer className="bg-gray-900 text-gray-300">
       {/* Top section */}
       <div className="max-w-7xl  mx-auto px-6 pt-8 pb-3">
-        <div className="flex flex-col gap-7 md:grid md:grid-cols-1 lg:grid-cols-2 mb-6">
+        <div className="flex flex-col gap-7 justify-between md:grid md:grid-cols-1 lg:grid-cols-2 mb-6">
           {/* Logo and Tagline */}
           <div className="flex flex-col items-center md:items-start" >
             <div className="flex items-center gap-2">
@@ -21,35 +21,26 @@ function Footer() {
             </p>
           </div>
           {/* Middle links */}
-          <div className="flex flex-col items-center md:items-start space-y-3">
-            <h3 className="text-lg font-semibold text-red-400">Quick Links</h3>
-            <a href="/" className="hover:text-red-400">
-              Home
-            </a>
+          <div className="flex flex-col items-center space-y-3 md:items-start md:pl-10 lg:pl-60">
             <a href="/about" className="hover:text-red-400">
               About Us
-            </a>
-            <a href="/talk" className="hover:text-red-400">
-              General Talk
-            </a>
-            <a href="/personality" className="hover:text-red-400">
-              Personality Check
             </a>
             <a href="/contact" className="hover:text-red-400">
               Contact Us
             </a>
           </div>
-          </div>
-
-          {/* Bottom bar */}
-          <div className="border-t border-gray-700 py-4">
-            <p className="text-center text-sm text-gray-400">
-              © {new Date().getFullYear()} Developer Anurag Yadav. All rights reserved.
-            </p>
-          </div>
         </div>
+
+        {/* Bottom bar */}
+        <div className="border-t border-gray-700 py-4">
+          <p className="text-center text-sm text-gray-400">
+            © {new Date().getFullYear()} Developer Anurag Yadav. All rights reserved.
+          </p>
+        </div>
+      </div>
     </footer>
   );
 }
 
 export default Footer;
+
