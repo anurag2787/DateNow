c5040355f0d8665fa6207f9e8572514cb2bc6006
anurag2787 <anuragyadav2787@gmail.com>
2025-09-30 20:15:26 +0530

    updated general talk

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
-import Cursor from "./components/Cursor/Cursor";
 import { Analytics } from "@vercel/analytics/react";
 import { AuthProvider } from "./context/AuthContext";
 import { ToastContainer } from 'react-toastify';
@@ -13,7 +12,6 @@ function Layout() {
       <div className="flex flex-col min-h-screen bg-[#F8A199]">
         <Analytics />
         <Navbar />
-        <Cursor />
         <main className="flex-grow">
           <Outlet />
         </main>
