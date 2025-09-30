9ebff4b2e78d4bf63e569a4dcf81ecc83e5d6fbb
anurag2787 <anuragyadav2787@gmail.com>
2025-09-30 20:13:22 +0530

    removed unwanted imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
